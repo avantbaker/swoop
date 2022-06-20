b6656b54a2e7248df16dba70cd3ca823d2a7ad39
Avant Baker <avantito@Avants-MacBook-Pro.local>
2022-06-20 18:10:36 -0500

    Moved App components into their own folders

---
+++ components/app-cards/index.tsx
@@ -0,0 +1,54 @@
+import { Section } from 'components/common/container';
+import styled from 'styled-components';
+import { Flex } from 'rebass/styled-components';
+import Text from 'components/common/text';
+
+const DualPathSection = styled('div')`
+	background: url(/elements/dotted-bg.svg);
+	border-top: 2px solid ${({ theme }) => theme.colors.orange};
+	border-bottom: 2px solid ${({ theme }) => theme.colors.orange};
+	padding: 0 !important;
+`;
+const DualSection = styled(Flex)`
+	background: ${({ theme }) => theme.colors.black};
+	border-left: 2px solid ${({ theme }) => theme.colors.orange};
+	border-right: 2px solid ${({ theme }) => theme.colors.orange};
+	align-items: flex-end;
+	flex: 1;
+	img {
+		height: 100%;
+	}
+	&:first-of-type {
+		border-bottom: 2px solid ${({ theme }) => theme.colors.orange};
+	}
+	&:hover {
+		background: rgba(231, 226, 218, 0.4);
+	}
+`;
+const VerticalTextL = styled(Text)`
+	writing-mode: vertical-lr;
+	-webkit-transform: rotate(-180deg);
+	-moz-transform: rotate(-180deg);
+`;
+export const AppCards = () => {
+	return (
+		<DualPathSection>
+			<Section
+				p={'0 !important'}
+				justifyContent="center"
+				flexDirection={['column', 'row']}
+			>
+				<DualSection justifyContent={['center']} py={[5, 6]}>
+					<VerticalTextL pl={4} variant="displayVertical">
+						Golfers
+					</VerticalTextL>
+					<img src="/elements/app-golfers-desktop.png" alt="" />
+				</DualSection>
+				<DualSection justifyContent={['center']} py={[5, 6]}>
+					<img src="/elements/app-courses-desktop.png" alt="" />
+					<VerticalTextL variant="displayVertical">Courses</VerticalTextL>
+				</DualSection>
+			</Section>
+		</DualPathSection>
+	);
+};
---
+++ components/text-intro/index.tsx
@@ -0,0 +1,19 @@
+import { Section } from 'components/common/container';
+import { Flex } from 'rebass/styled-components';
+import Text from 'components/common/text';
+import Pin from 'components/common/icons/pin';
+import { rem } from 'polished';
+
+export const TextIntro = () => {
+	return (
+		<Section justifyContent="center" mb={5}>
+			<Flex flexDirection="column" alignItems="center" mt={[6]}>
+				<Pin />
+				<Text variant="h2" textAlign="center" maxWidth={rem(573)} mt={[4]}>
+					Byrdi is giving courses and golfers control over concessions from the tee to the
+					turn, and on.
+				</Text>
+			</Flex>
+		</Section>
+	);
+};
